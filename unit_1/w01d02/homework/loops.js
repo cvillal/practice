db4640d5147f8956bd85a7412235bfbcae8a26fa
Carolina Villalobos <cvillal@ncsu.edu>
2022-07-12 23:22:47 -0400

    everythings a mess so im trying to commit all at once

---
+++ unit_1/w01d02/homework/loops.js
@@ -0,0 +1,48 @@
+// Create a loop that logs the numbers from 0-99 (ascending)
+    // for(let i=0; i<=100; i++){
+    //     console.log(i);
+    // }
+// Create a loop that logs the numbers from 99-0 (descending)
+    // for(let i=99; i>=0; i--){
+    //     console.log(i);
+    // }
+    
+// Create a loop that logs the EVEN numbers from 0-98 (ascending)
+//     let num=0;
+//     while (num<=98){
+//     console.log(num+=2);
+// }
+    
+
+// Create a loop that logs the EVEN numbers from 98-0 (descending)
+    // let num=98;
+    // // while (num>=0){
+    // // console.log(num%2);
+    // // }   
+
+    //this ran a forever loop and i dotn know what to do 
+
+// Create a loop that logs the ODD numbers from 0-99 (ascending)
+// for(let i=0; i<=99; i%2 !=0){
+//         console.log(i);
+//     }
+
+// // Create a loop that logs the ODD numbers from 99-0 (descending)
+// for(let i=99; i>=0; i--){
+//     //     console.log(i);
+//     // }
+
+// // Create a loop that logs the numbers from 49-72 (ascending)
+// for(let i=99; i>=0; i--){
+//     //     console.log(i);
+//     // }
+
+// // Create a loop that logs the numbers from 81-26 (descending)
+// for(let i=99; i>=0; i--){
+//     //     console.log(i);
+//     // }
+
+// // Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
+// for(let i=99; i>=0; i--){
+//     //     console.log(i);
+//     // }
