3e4d2e108598c700f7984544aadba8d532b006c6
Carolina Villalobos <cvillal@ncsu.edu>
2022-07-14 17:48:22 -0400

    attempted but incomplete palindrome - will come back to it

--- unit_1/w01d04/homework/solutions.js
+++ unit_1/w01d04/homework/solutions.js
@@ -1,3 +1,4 @@
+//Verbal Questions:
 // What is the difference between a parameter and an argument?
 // a Parameter modifies the behavior of the function.
 // An Argument specifies the value of the parameter. (what is "plugged in" as the parameter)
@@ -5,3 +6,18 @@
 // Within a function, what is the difference between return and console.log?
 // Return is what will be inputted as the argument. (ends the function immediately)
 // console.log prints the argument.
+
+//Palindrome:
+// const checkPalindrome=("") {
+//     if(""===palindrome){
+//         console.log("".split().reverse().join())
+//     }
+//     console.log (checkPalindrome("");
+//     )
+
+// }
+
+//unsure where to start. unsure how to set it up and what to put where. am i just looking for true/false? or that the function works and returns the words reversed adn w a capital letter upfront?
+
+
+
