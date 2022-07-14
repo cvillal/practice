4ce6cd3d8157c3c679fb24995891a70699375b38
Carolina Villalobos <cvillal@ncsu.edu>
2022-07-13 21:37:27 -0400

    wild wild life answered

--- unit_1/w01d03/homework/.answers.js
+++ unit_1/w01d03/homework/.answers.js
@@ -19,22 +19,49 @@
 ////////////////////////////////
 // Fizz Buzz:
 
-for (let i=1; i<=100; i++){
-    if (i % 3 === 0){
-    console.log(Fizz)
-    }
-    if (i % 5 === 0){
-        console.log(Buzz)
-    }
-    if (i % 3 ===0 && i % 5 === 0){
-        console.log(FizzBuzz)
-    }
-}
+// for (let i=1; i<=100; i++){
+//     if (i % 3 === 0){
+//     console.log(Fizz)
+//     }
+//     if (i % 5 === 0){
+//         console.log(Buzz)
+//     }
+//     if (i % 3 ===0 && i % 5 === 0){
+//         console.log(FizzBuzz)
+//     }
+// }
+
+//UNSURE WHAT TO DO...RUNNING IT SAYS I NEED TO DEFINE FUZZ BUT I DONT KNOW WHAT TO DEFINE IT AS
+
 ////////////////////////////////
 
 
 ////////////////////////////////
 // Wild Wild Life
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+// 1.
+// plantee [2] =5001
+// console.log(plantee)
+// 2.
+// wolfy [3] = "Gotham City"
+// console.log(wolfy)
+// 3.
+// dart.push("Hawkins")
+// console.log(dart)
+// 4.
+// wolfy[0]="Gameboy"
+// console.log (wolfy)
+
+
+
+
+
+
 ////////////////////////////////
 
 
