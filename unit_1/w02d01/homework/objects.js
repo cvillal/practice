f076687d4e1b3e5af60881d3376633c92c7f805c
Carolina Villalobos <cvillal@ncsu.edu>
2022-07-18 21:05:58 -0400

    part one complete

---
+++ unit_1/w02d01/homework/objects.js
@@ -0,0 +1,42 @@
+// const setUp ={
+//     table: "round",
+//     screens: 2,
+//     laptop: "Macbook Pro",
+//     wirelessAccessories: "mouse",
+//     notebook: "spiral grid",
+//     pencilCase: "yellow cylinder",
+//     pencil: "mechanical .7",
+//     pens: ["green", "yellow", "red", "dark blue"]
+// }
+
+// setUp.clutter=["hair-clip","chapstick","sunscreen","dog picture","sticker","lavender oil","gems","waterbottle"];
+
+// console.log (setUp)
+
+//4.
+//console.log(setUp.clutter)
+
+// 5.
+// setUp.computer={screenSize:"OS",};
+
+// //6.
+// // console.log(setUp.computer)
+
+//7.
+// setUp.computer.files= ["homework","labs","notes", "trash"]
+
+// console.log(setUp)
+
+// //8.
+// console.log(setUp.computer.files)
+
+// //9.
+// const books = [{title: 'Pragmatic Programmer', author: 'David Thomas + Andrew Hunt'}, {title: 'Clean Code', author: 'Robert Martin'}, {title: 'You Don\'t Know JS', author: 'Kyle Simpson'}]
+
+// for (let i=0; i<books.length; i++){
+//     console.log(`${books[i].title} by ${books[i].author}`)
+// }
+
+//PART2:Hero vs Goblin
+
+
