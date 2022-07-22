eae356ed034ba603dd294886ec42fc5e36c19535
Carolina Villalobos <cvillal@ncsu.edu>
2022-07-21 23:23:16 -0400

    Commit3 I TRIED TO ADD list elements wasnt very successful. that code didnt seem very dry.i wonder if there is something i could do about it in the hungry for more section.

--- unit_1/w02d04/homework/hogwarts/app.js
+++ unit_1/w02d04/homework/hogwarts/app.js
@@ -6,6 +6,7 @@ let $name= $('<h2>')
 let $house= $('<h3>')
 let $pet= $('<h4>')
 let $wand= $('<h4>')
+let $trunk= $('<ul>')
 //change
 $h1.text('Welcome to Hogwarts!')
 $name.text('Carolina')
@@ -13,7 +14,10 @@ $house.text('Hufflepuff')
 $pet.addClass('Owl').text('Chicken')
 $wand.text('Holly Wand with Unicorn Hair Core')
 
+$trunk.attr('storage','trunk')
+$trunk.html('<li>Butter Beer</li>','<li>Invisibility Cloak</li>','<li>Magic Map</li>','<li>Time Turner</li>','<li>Leash</li>',"<li>Bertie Bott's Every Flavor[Jelly]Beans</li>")
 
+console.log($trunk)
 // console.log($container)
 // console.log($h1)
 //append
@@ -22,7 +26,7 @@ $container.append($name)
 $container.append($house)
 $container.append($pet)
 $container.append($wand)
-
+$container.append($trunk)
 
 
 });
