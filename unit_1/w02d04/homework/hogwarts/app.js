b597199f5cd440536c146191e9ecf9ea0a954802
Carolina Villalobos <cvillal@ncsu.edu>
2022-07-21 21:48:21 -0400

    Commit2 I added more elements. My powers frow with my dedication and hard work. yes the do

--- unit_1/w02d04/homework/hogwarts/app.js
+++ unit_1/w02d04/homework/hogwarts/app.js
@@ -2,11 +2,27 @@ $(() => {
 //create
 let $container= $('#container')
 let $h1= $('<h1>')
+let $name= $('<h2>')
+let $house= $('<h3>')
+let $pet= $('<h4>')
+let $wand= $('<h4>')
 //change
 $h1.text('Welcome to Hogwarts!')
+$name.text('Carolina')
+$house.text('Hufflepuff')
+$pet.addClass('Owl').text('Chicken')
+$wand.text('Holly Wand with Unicorn Hair Core')
+
+
 // console.log($container)
-console.log($h1)
+// console.log($h1)
 //append
 $container.append($h1)
+$container.append($name)
+$container.append($house)
+$container.append($pet)
+$container.append($wand)
+
+
 
 });
