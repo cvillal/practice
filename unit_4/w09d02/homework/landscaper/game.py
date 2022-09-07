e22e0b0149abed9f9dc7ee39beb53ba9112cf813
Carolina Villalobos <cvillal@ncsu.edu>
2022-09-06 21:23:21 -0400

    set up player and too class attempt

---
+++ unit_4/w09d02/homework/landscaper/game.py
@@ -0,0 +1,30 @@
+
+name = input('Whats your name?')
+
+class Player:
+    def __init__(self, name, tool, money):
+        self.name = name
+        self.tool = tool
+        self.money = money
+
+class Tool:
+    def __init__(self, tool)
+        self.name = name
+        self.tool = tool[]
+
+tool= [
+    ['teeth', 'rusty scissors', 'OT push lawnmower', 'BP lawnmower', 'starvoing students'],
+    [1, 5, 50, 100, 250],
+    [0, 5, 25, 250, 500]
+    ]
+
+[ "teeth", "rusty scissors", "old timey lawnmower"]
+    
+# ]
+
+# def 
+# rusty_scissors = Tool()
+
+player = Player(name, 'teeth', "$1")
+
+print(player)
