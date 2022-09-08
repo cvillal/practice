af94256f5c58cd04de9f8659060050c296dce7dd
Carolina Villalobos <cvillal@ncsu.edu>
2022-09-07 21:31:11 -0400

    seeded data and NFL queries 1 done

--- unit_4/w09d03/homework/nfl/nfl.sql
+++ unit_4/w09d03/homework/nfl/nfl.sql
@@ -1,11 +1,74 @@
 -- 1. List the names of all NFL teams'
+nfl=# SELECT name FROM teams;
+         name         
+----------------------
+ Buffalo Bills
+ Miami Dolphins
+ New England Patriots
+ New York Jets
+ Baltimore Ravens
+ Cincinnati Bengals
+ Cleveland Browns
+ Pittsburgh Steelers
+ Houston Texans
+ Indianapolis Colts
+ Jacksonville Jaguars
+ Tennessee Titans
+ Denver Broncos
+ Kansas City Chiefs
+ Oakland Raiders
+ San Diego Chargers
+ Dallas Cowboys
+ New York Giants
+ Philadelphia Eagles
+ Washington Redskins
+ Chicago Bears
+ Detroit Lions
+ Green Bay Packers
+ Minnesota Vikings
+ Atlanta Falcons
+ Carolina Panthers
+ New Orleans Saints
+ Tampa Bay Buccaneers
+ Arizona Cardinals
+ St. Louis Rams
+ San Francisco 49ers
+ Seattle Seahawks
 
 -- 2. List the stadium name and head coach of all NFC teams
+nfl=# SELECT stadium, head_coach FROM teams WHERE conference = 'NFC';
+            stadium            |  head_coach   
+-------------------------------+---------------
+ AT&T Stadium                  | Jason Garrett
+ MetLife Stadium               | Tom Coughlin
+ Lincoln Financial Field       | Chip Kelly
+ FedExField                    | Jay Gruden
+ Soldier Field                 | Marc Trestman
+ Ford Field                    | Jim Caldwell
+ Lambeau Field                 | Mike McCarthy
+ TCF Bank Stadium              | Mike Zimmer
+ Georgia Dome                  | Mike Smith
+ Bank of America Stadium       | Ron Rivera
+ Mercedes-Benz Superdome       | Sean Payton
+ Raymond James Stadium         | Lovie Smith
+ University of Phoenix Stadium | Bruce Arians
+ Edward Jones Dome             | Jeff Fisher
+ Levis Stadium                 | Jim Harbaugh
+ CenturyLink Field             | Pete Carroll
 
 -- 3. List the head coaches of the AFC South
-
+nfl=# SELECT head_coach FROM teams WHERE conference = 'AFC' AND division = 'South';
+   head_coach   
+----------------
+ Bill OBrien
+ Chuck Pagano
+ Gus Bradley
+ Ken Whisenhunt
 -- 4. The total number of players in the NFL
-
+nfl=# SELECT COUNT(*) FROM players;
+ count 
+-------
+  1532
 -- 5. The team names and head coaches of the NFC North and AFC East
 
 -- 6. The 50 players with the highest salaries
