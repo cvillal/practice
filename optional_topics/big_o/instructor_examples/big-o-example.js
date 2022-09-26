ccc3bfe358e79baab32867145c49eeb7868be130
Matt Huntington <matt.huntington@gmail.com>
2022-09-26 09:58:31 -0400

    optional topics

---
+++ optional_topics/big_o/instructor_examples/big-o-example.js
@@ -0,0 +1,15 @@
+const compareElems = (arrayParam) => {
+
+
+    for(let i = 0; i < arrayParam.length; i++){
+
+
+        for(let j = 0; j < arrayParam.length; j++){
+
+
+            console.log(arrayParam[i], arrayParam[j]);
+        }
+    }
+}
+
+compareElems(['a','b','c','d'])
---
+++ optional_topics/data-structures/instructor_examples/data_structures.js
@@ -0,0 +1,62 @@
+// const firstFunc = () => {
+//     const foo = 'bar';
+//     secondFunc()
+//     const matt = 'huntington';
+// }
+//
+// const secondFunc = () => {
+//     const awesome = true;
+// }
+//
+// firstFunc()
+
+// const stack = [];
+// stack.push(1);
+// stack.push(9);
+// stack.push(5);
+// console.log(stack);
+// stack.pop();
+// console.log(stack);
+//
+// const queue = [];
+// queue.push(1);
+// queue.push(9);
+// queue.push(5);
+// console.log(queue);
+// queue.shift();
+// console.log(queue);
+
+// const head = {
+//     name:'Matt'
+// }
+//
+// const person2 = {
+//     name: 'bob'
+// }
+//
+// const person3 = {
+//     name: 'Sally'
+// }
+//
+// const tail = {
+//     name: 'Zagthorp'
+// }
+//
+//
+//
+// const find = (node, value) => {
+//     if(node.name === value){
+//         return true;
+//     } else if (node.next == undefined){
+//         return false;
+//     } else {
+//         return find(node.next, value);
+//     }
+// }
+//
+// head.next = person2;
+// person2.next = person3;
+// person3.next = tail;
+//
+// console.log(find(head, 'Zagthorp'));
+// // console.log(head);
